ea3601e4043a31eed56d57099b542323d9bc6e0b
hadrienmp <public@hadrienmp.fr>
2020-05-29 20:04:13 +0200

    Add a result tag in the parse response

--- test/status_test.erl
+++ test/status_test.erl
@@ -4,5 +4,6 @@
 'can be parsed from the server status json_test'() -> 
     ?assertMatch({ok, #{timeLeft:={1000,ms}}}, 
                  status:parse("{\"timeLeftInMillis\":1000}")).
+
 'returns an error for an invalid response_test'() ->
     ?assertMatch({error, _}, status:parse("Ooops")).
