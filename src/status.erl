a26596e1c2d19ee3392529150e1c785cb1d1dc09
hadrienmp <public@hadrienmp.fr>
2020-05-29 20:02:54 +0200

    Add a result tag in the parse response

--- src/status.erl
+++ src/status.erl
@@ -2,9 +2,8 @@
 -export([parse/1]).
 
 parse(Json) -> 
-    Decoded = jsone:decode(list_to_binary(Json)),
-    {ok, 
-     #{timeLeft => 
-      {maps:get(<<"timeLeftInMillis">>, Decoded),ms}
-     }
-    }.
+    Result = jsone:try_decode(list_to_binary(Json)),
+    parseResult(Result).
+
+parseResult({ok, Decoded, _}) -> {ok, #{timeLeft => {maps:get(<<"timeLeftInMillis">>, Decoded),ms}}};
+parseResult({_, Reason}) -> {error, {"Invaid json", {reason, Reason}}}.
--- test/status_test.erl
+++ test/status_test.erl
@@ -4,4 +4,5 @@
 'can be parsed from the server status json_test'() -> 
     ?assertMatch({ok, #{timeLeft:={1000,ms}}}, 
                  status:parse("{\"timeLeftInMillis\":1000}")).
-
+'returns an error for an invalid response_test'() ->
+    ?assertMatch({error, _}, status:parse("Ooops")).
