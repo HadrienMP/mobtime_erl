9bc0bc197c0f48658eacade06a4273521d29846e
Hadrien Mens-Pellen <hmens@financeactive.com>
2020-06-04 11:01:19 +0200

    Improve infinitest

--- infinitest.sh
+++ infinitest.sh
@@ -3,12 +3,12 @@
 function test() {
     clear
     rebar3 eunit --module=$1
-    echo -e "\n"
 }
 
+export -f test
+test
 if [[ $1 = "mac" ]]; then
-    fswatch -o --exclude=_build . | xargs -n1 -I{} sh -c 'clear && rebar3 eunit'
+    fswatch -o . --exclude=_build --exclude="\.eunit" | xargs -n1 -I{} test
 else
-    export -f test
     fswatch -m poll_monitor -o -r -0 . -e "\..*$" -e "_build" -e "\.eunit" | xargs -0 -n 1 -I{} -r bash -c "test $1"
 fi
